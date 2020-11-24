86ede0dd4131575a5bd2e58b73136588c39d9a7e
zeyanjie <zeyanjie@sjtu.edu.cn>
2020-11-24 19:30:59 +0800

    train finished

--- pcdataloader.py
+++ pcdataloader.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def loadnpy():
+def loadnpy1():
     data = np.load("./hw/0.npy")
     data = data.T
     data = np.expand_dims(data, axis=0)
@@ -26,12 +26,32 @@ def loadnpy():
     
     return data
 
+def loadnpy2():
+    data = np.load("./hw/0.npy")
+  
+    data = np.expand_dims(data, axis=0)
+    judge = 1
+    for i in range(19):
+
+        if judge ==1:
+            i=i+1
+            judge+=1
+
+        filename  ="./hw/%d.npy"%i
+        a = np.load(filename)
+       
+        a= np.expand_dims(a,axis=0)
+        data = np.vstack([data,a])
+    
+    return data
+
 # point cloud dataset
 class pcdataset(Dataset):
 
     def __init__(self, transform=None,split='train'):
-        self.data = loadnpy()
+        self.data = loadnpy1()
         self.transform = transform
+        self.label = loadnpy2()
 
     def __getitem__(self, idx):
 
@@ -39,23 +59,30 @@ class pcdataset(Dataset):
             idx = idx.tolist()
 
         data_i = self.data[idx]
+        label_i = self.label[idx]
         
         if self.transform:
             data_i = self.transform(data_i)
-        return data_i
+        return data_i,label_i
 
     def __len__(self):
         return self.data.shape[0]
 
-
+def pc_normalize(pc):
+    l = pc.shape[0]
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
+    pc = pc / m
+    return pc
 
 
 if __name__ =="__main__":
     
     set = pcdataset()
     loader = DataLoader(set, batch_size=4, shuffle=True)
-    for i,da in enumerate(loader):
-        print(da.shape)
+    for i,(x,y) in enumerate(loader):
+        print(x.shape,y.shape)
         break
 
 
--- train.py
+++ train.py
@@ -1,41 +1,51 @@
-from pcdataloader import pcdataset,DataLoader
+from pcdataloader import pcdataset,DataLoader,pc_normalize
 from partnet import partnet
 from semnet import semnet
 import torch
 import torch.optim
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import os
 
-learning_rate = 0.001
+learning_rate = 1e-5
 batch_size = 5
 part_num = 1
 validation_split = 0.2
 shuffle_dataset = True
+epoch = 50
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+model = semnet().float().to(device)
 
-model = semnet()
+if os.path.exists('params.pkl'):
+    model.load_state_dict(torch.load('params.pkl'))
 
 optimizer = torch.optim.Adam(
             model.parameters(),
             lr=learning_rate,
             eps=1e-08,
-            weight_decay=True
         )
 
-dataset = pcdataset(transform=None)
+dataset = pcdataset(transform=pc_normalize)
 
-if torch.cuda.is_available():
-    devide = torch.device("cuda")
-    
-
-"""
-part segmentation任务的输出是什么？
-到底什么是part segmentation？
-
-还剩下未做的：
-数据预处理
-数据集的label没有设置
-搞清楚什么part segmentation
-进行训练
-"""
-
-
+loss_fn = nn.MSELoss()
 loader = DataLoader(dataset,batch_size)
+
+
+
+
+model.train()
+for e in range(epoch):
+    for idx,(xtrain,ytrain) in enumerate(loader):
+        
+        x_train = xtrain.float().to(device)
+        y_train = ytrain.float().to(device)
+
+        result,_ = model(x_train)
+        loss = loss_fn(result,y_train)
+        print("epoch: ",e," idx: ",idx, " loss:", loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+torch.save(model.state_dict(), 'params.pkl')
